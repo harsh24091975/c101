efa0a5de56cbed64482d910b8118f9f13c332091
harsh24091975 <74312669+harsh24091975@users.noreply.github.com>
2021-10-20 12:48:10 +0530

    Update cloud_storage.py

--- cloud_storage.py
+++ cloud_storage.py
@@ -1,38 +1,25 @@
 import dropbox
-import os
 
 class TransferData:
     def __init__(self, access_token):
         self.access_token = access_token
-    
-    def upload_files(self, file_from, file_to):
-        for root, dirs, files in os.walk(file_from):
-            relative_path = os.path.relpath(local_path, file_from)
-            dropbox_path = os.path.join(file_to, relative_path)
 
-        with open(local_path, 'rb') as f:
-            dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
 
 
-
-
-
-
-
+        f = open(file_from, 'rb')
+        dbx.files_upload(f.read(), file_to)
 
 def main():
-    access_token = "sl.A6k3G7sRwyArNQpGLbwCKjYogdc5GqPPPT3EoFcrOpV6cPsX5QXD3IKvUoF2AcD13P9E7zmP6nSHifduWfUFvSBTdJcxkc-ouAK_rNVl0tJO_twFNqUPEE1RZjpfg45_-XScBeE"
+    access_token = 'sl.A6vrSWUy-P9lutic18kLmjw3gt8kin7RcFaSWBovVstgsmYoijLitCCfThq_BjNIRs3HCN9VBvOfgv03NZl8KJv8RJjunIX5_R1ERRenEw6CTyawsXTwZ4bFYYfV-tTL5ojjg-I'
     transferData = TransferData(access_token)
 
-    file_from = input("Enter the file path to transfer:- ")
-    file_to = input("Enter the full path to upload to dropbox:- ")
-    transferData.upload_files(file_from, file_to)
-    print("File has been successfully uploaded.")
+    file_from = input("Enter the file path to transfer : -")
+    file_to = input("enter the full path to upload to dropbox:- ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
 
-
-
-
-
+    transferData.upload_file(file_from, file_to)
+    print("file has been moved !!!")
 
 
 main()
