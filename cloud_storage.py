03d1f27d31a43f49526085b8941130f7e4e9b984
harsh24091975 <74312669+harsh24091975@users.noreply.github.com>
2021-10-22 14:19:33 +0530

    Update cloud_storage.py

--- cloud_storage.py
+++ cloud_storage.py
@@ -1,4 +1,6 @@
 import dropbox
+import os 
+from dropbox.files import WriteMode
 
 class TransferData:
     def __init__(self, access_token):
@@ -7,10 +9,14 @@ class TransferData:
     def upload_file(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
 
-
-        f = open(file_from, 'rb')
-        dbx.files_upload(f.read(), file_to)
-
+        for root,dirs,files in os.walk(file_from):
+            for filename in file:
+                lpath=os.path.join(root,filename)
+                rpath=os.path.relpath(lpath,file_from)
+                dpath=os.path.join(file_to,rpath)
+                with open(lpath,'rb') as f:
+                    dbx.files_upload(f.read(), dpath,mode=WriteMode('overwrite'))
+                    
 def main():
     access_token = 'sl.A6vrSWUy-P9lutic18kLmjw3gt8kin7RcFaSWBovVstgsmYoijLitCCfThq_BjNIRs3HCN9VBvOfgv03NZl8KJv8RJjunIX5_R1ERRenEw6CTyawsXTwZ4bFYYfV-tTL5ojjg-I'
     transferData = TransferData(access_token)
